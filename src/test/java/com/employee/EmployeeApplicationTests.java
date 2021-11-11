648e39ce464915a2bfb64aa976f41d299ecb4be7
Naredla <Sailu.Naredla@transformco.com>
2021-11-11 12:26:26 +0530

    read.me file updared

--- src/test/java/com/employee/EmployeeApplicationTests.java
+++ src/test/java/com/employee/EmployeeApplicationTests.java
@@ -1,10 +1,10 @@
-package com.javatpoint;
+package com.employee;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBootH2DatabaseExampleApplicationTests {
+class EmployeeApplicationTests {
 
 	@Test
 	void contextLoads() {
